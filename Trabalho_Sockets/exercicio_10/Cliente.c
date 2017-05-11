f6d1bfe4f01eccf73c3f5246edc4bb29fb02d698
Rhoger Anacleto <rhogeranacleto@gmail.com>
2017-05-11 19:30:57 +0000

    Exercicio 10

--- Trabalho_Sockets/exercicio_10/Cliente.c
+++ Trabalho_Sockets/exercicio_10/Cliente.c
@@ -20,9 +20,9 @@ int main(int argc , char *argv[])
     }
     puts("Socket created");
      
-    server.sin_addr.s_addr = inet_addr("ip do servidor");
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
-    server.sin_port = htons( 8080 );
+    server.sin_port = htons( 8888 );
  
     //Connect to remote server
     if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
