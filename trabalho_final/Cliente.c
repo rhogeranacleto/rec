f37dff507907f4b227d1ed52244e706ef0ef28de
Rhoger Anacleto <rhogeranacleto@gmail.com>
2017-06-20 04:23:28 +0000

    trabalho terminado praticamente

--- trabalho_final/Cliente.c
+++ trabalho_final/Cliente.c
@@ -1,62 +1,123 @@
 /*
-    C ECHO client example using sockets
+	C ECHO client example using sockets
 */
 #include<stdio.h> //printf
 #include<string.h>    //strlen
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
+#include <sys/time.h>
+
+int try_send(int sock, char* message, int sequence, int time_execution);
  
 int main(int argc , char *argv[])
 {
-    int sock;
-    struct sockaddr_in server;
-    char message[1000] , server_reply[2000];
-     
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-     
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 8888 );
+	int sock, recv_size;
+	struct sockaddr_in server;
+	char message[1000];
+	 
+	//Create socket
+	sock = socket(AF_INET , SOCK_STREAM , 0);
+	if (sock == -1)
+	{
+		printf("Could not create socket");
+	}
+	puts("Socket created");
+	 
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons( 8888 );
  
-    //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
-        return 1;
-    }
-     
-    puts("Connected\n");
-     
-    //keep communicating with server
-    while(1)
-    {
-        printf("Enter message : ");
-        fgets(message, 100, stdin);
-         
-        //Send some data
-        if( send(sock, message , strlen(message) , 0) < 0)
-        {
-            puts("Send failed");
-            return 1;
-        }
-         
-        //Receive a reply from the server
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
-         
-        puts("Server reply :");
-        puts(server_reply);
-    }
-     
-    close(sock);
-    return 0;
+	//Connect to remote server
+	if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		perror("connect failed. Error");
+		return 1;
+	}
+	 
+	puts("Connected\n");
+	
+	struct timeval timeout;
+	 timeout.tv_sec = 10;
+	 timeout.tv_usec = 0;
+	
+	//keep communicating with server
+	while(1)
+	{
+		printf("Enter message : ");
+		fgets(message, 100, stdin);
+
+		int r = try_send(sock, message, 1234, 1);
+		
+		if(r != 0) {
+			
+			break;
+		}
+	}
+	 
+	close(sock);
+	return 0;
+}
+
+int try_send(int sock, char* message, int sequence, int time_execution) {
+
+	int len = strlen(message);
+	int recv_size;
+	char buf[120], server_reply[2000];
+
+	sprintf(buf, "%d_%d", sequence, len);
+
+	if(send(sock, buf, strlen(buf) , 0) >= 0) {
+			
+		struct timeval timeout;
+		timeout.tv_sec = 1 / 2;
+		timeout.tv_usec = 0;
+ 
+		fd_set in_set;
+
+		FD_ZERO(&in_set);
+		FD_SET(sock, &in_set);
+
+		// select the set
+		int cnt = select(sock + 1, &in_set, NULL, NULL, &timeout);
+		
+		if (FD_ISSET(sock, &in_set)) {
+
+			//Receive a reply from the server
+			if((recv_size = recv(sock , server_reply , 2000 , 0)) >= 0) {
+
+				if(send(sock, message, strlen(message) , 0) >= 0) {
+				memset(server_reply, 0, sizeof server_reply);
+					if(recv(sock, server_reply , 2000 , 0) >= 0) {
+					
+						puts("Server reply :");
+						puts(server_reply);
+						return 0;
+					} else {
+						
+						puts("error receive message");
+						
+						return 1;
+					}
+				} else {
+					
+					puts("error send message");
+					
+					return 1;
+				}
+			} else {
+			
+				puts("recv failed" + recv_size);
+				return recv_size;
+			}
+		} else {
+			
+			printf("tentativa %d\n", time_execution);
+			
+			return try_send(sock, message, sequence, time_execution + 1);
+		}
+	} else {
+
+		puts("Send failed");
+		return 1;
+	}
 }
--- trabalho_final/Servidor.c
+++ trabalho_final/Servidor.c
@@ -1,5 +1,5 @@
 /*
-    C socket server example
+	C socket server example
 */
  
 #include<stdio.h>
@@ -7,67 +7,80 @@
 #include<sys/socket.h>
 #include<arpa/inet.h> //inet_addr
 #include<unistd.h>    //write
+#include <string.h>
  
 int main(int argc , char *argv[])
 {
-    int socket_desc , client_sock , c , read_size;
-    struct sockaddr_in server , client;
-    char client_message[2000];
-     
-    //Create socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-     
-    //Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
-     
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        //print the error message
-        perror("bind failed. Error");
-        return 1;
-    }
-    puts("bind done");
-     
-    //Listen
-    listen(socket_desc , 3);
-     
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
-     
-    //accept connection from an incoming client
-    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
-    if (client_sock < 0)
-    {
-        perror("accept failed");
-        return 1;
-    }
-    puts("Connection accepted");
-     
-    //Receive a message from client
-    while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
-    {
-        //Send the message back to client
-        write(client_sock , client_message , strlen(client_message));
-    }
-     
-    if(read_size == 0)
-    {
-        puts("Client disconnected");
-        fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
-    }
-     
-    return 0;
+	int socket_desc , client_sock , c , read_size;
+	struct sockaddr_in server , client;
+	char client_message[2000];
+	 
+	//Create socket
+	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	if (socket_desc == -1)
+	{
+		printf("Could not create socket");
+	}
+	puts("Socket created");
+	 
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons( 8888 );
+	 
+	//Bind
+	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		//print the error message
+		perror("bind failed. Error");
+		return 1;
+	}
+	puts("bind done");
+	 
+	//Listen
+	listen(socket_desc , 3);
+	 
+	//Accept and incoming connection
+	puts("Waiting for incoming connections...");
+	c = sizeof(struct sockaddr_in);
+	 
+	//accept connection from an incoming client
+	client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+	if (client_sock < 0)
+	{
+		perror("accept failed");
+		return 1;
+	}
+	puts("Connection accepted");
+	 
+	//Receive a message from client
+	while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
+	{
+		
+		char * pch;
+		pch = strtok (client_message, "_");
+		
+		if(pch) {
+		
+			int sequence = atoi(pch);
+			
+			sprintf(client_message, "%s_%d", pch, sequence+1);
+		}
+			
+			puts(client_message);
+		//Send the message back to client
+		write(client_sock , client_message , strlen(client_message));
+	}
+	 
+	if(read_size == 0)
+	{
+		puts("Client disconnected");
+		fflush(stdout);
+	}
+	else if(read_size == -1)
+	{
+		perror("recv failed");
+	}
+	 
+	return 0;
 }
